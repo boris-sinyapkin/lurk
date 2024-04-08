2bd1262882986d508f829e83aa4655214eb28191
Boris Sinyapkin <bsinyapkin@azul.com>
2024-04-08 19:44:20 +0300

    NFC: format main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,16 @@ use std::net::{IpAddr, SocketAddr};
 async fn main() -> Result<()> {
     // Initialize logging
     log4rs::init_file(config::LOG4RS_CONFIG_FILE_PATH, Deserializers::default()).unwrap();
-    // Parse config
+
+    // Parse server config
     let config = LurkConfig::parse();
-    // Create server
+
+    // Create proxy server instance. It will handle incoming connection in async. fashion.
     let server_addr = SocketAddr::new(IpAddr::V4(config.ipv4()), config.port());
     let server = LurkServer::new(server_addr, config.auth_enabled());
+
     // Bind and serve clients "forever"
     server.run().await?;
+
     Ok(())
 }
