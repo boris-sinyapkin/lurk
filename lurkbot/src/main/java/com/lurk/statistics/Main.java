40fa1d3090db3a31227c97a8ce01908a6d6623ab
Boris S <boris.works@hotmail.com>
2024-06-29 00:10:48 +0300

    [LurkBot] Move config to LurkBot instance

--- lurkbot/src/main/java/com/lurk/statistics/Main.java
+++ lurkbot/src/main/java/com/lurk/statistics/Main.java
@@ -1,10 +1,5 @@
 package com.lurk.statistics;
 
-import java.io.IOException;
-import java.net.URL;
-import io.smallrye.config.PropertiesConfigSource;
-import io.smallrye.config.SmallRyeConfig;
-import io.smallrye.config.SmallRyeConfigProviderResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
@@ -14,23 +9,11 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-        LurkBotConfiguration config = getBotConfiguration();
+        LurkBotConfiguration config = LurkBot.getConfig();
 
         @SuppressWarnings("resource")
         TelegramBotsLongPollingApplication application = new TelegramBotsLongPollingApplication();
 
         application.registerBot(config.telegramBotToken(), new LurkBot());
-    }
-
-    private static LurkBotConfiguration getBotConfiguration() throws IOException {
-        URL url = Main.class.getResource("/application.properties");
-        if (url == null) {
-            throw new IOException("application.properties not found");
-        }
-        SmallRyeConfig config = new SmallRyeConfigProviderResolver().getBuilder()
-                .withMapping(LurkBotConfiguration.class)
-                .withSources(new PropertiesConfigSource(url))
-                .build();
-        return config.getConfigMapping(LurkBotConfiguration.class);
     }
 }
