03d8a9f54f1cdebaf872cae6d06ae79bbc78004a
Boris S <boris.works@hotmail.com>
2024-07-11 17:48:02 +0300

    [LurkBot] Remove left-overs in LurkUtils

--- lurkbot/src/main/java/com/lurk/statistics/LurkUtils.java
+++ lurkbot/src/main/java/com/lurk/statistics/LurkUtils.java
@@ -1,6 +1,5 @@
 package com.lurk.statistics;
 
-import java.net.http.HttpResponse;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public class LurkUtils {
@@ -10,17 +9,6 @@ public class LurkUtils {
         MARKDOWN,
         HTML
     };
-
-    public static String buildHealthcheckStatusString(LurkNode node, String status) {
-        return String.format("%s: %s\n", node.toString(), status);
-    }
-
-    public static String buildHealthcheckStatusString(LurkNode node, HttpResponse<?> httpResponse) {
-        String nodeStatus = (httpResponse.statusCode() == 200)
-                ? "is up and running"
-                : "is died";
-        return buildHealthcheckStatusString(node, nodeStatus);
-    }
 
     public static SendMessage buildMessageWithText(long chatId, String format, Object... args) {
         return buildMessageWithText(chatId, String.format(format, args));
