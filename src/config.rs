e98ed10f931681d4552ff579c6c5f27a0157492b
Boris S. <boris.works@hotmail.com>
2024-07-25 16:21:27 +0300

    Introduce HTTP endpoint for management (#29)
    This adds an initial implementation of management HTTP endpoint serving requests in a separate thread.
    Currently, it just handle only 1 path (/healthcheck) and returns HTTP_OK as a response.
    It has separate config structure and disabled by default.

--- src/config.rs
+++ src/config.rs
@@ -6,20 +6,51 @@ pub const LOG4RS_CONFIG_FILE_PATH: &str = "log4rs.yaml";
 #[derive(Default, Parser, Debug)]
 #[clap(author = "Boris S. <boris.works@hotmail.com>", about = "Fast and fancy SOCKS5 proxy", version)]
 pub struct LurkConfig {
-    /// Proxy TCP port to listen on
-    #[clap(short = 'p', long, default_value_t = 1080)]
+    #[command(flatten)]
+    proxy_server_config: LurkProxyServerConfig,
+
+    #[command(flatten)]
+    http_endpoint_config: LurkHttpEndpointConfig,
+}
+
+#[derive(Default, Parser, Debug)]
+struct LurkHttpEndpointConfig {
+    /// Spin up HTTP endpoint in a background thread
+    #[arg(long, default_value_t = false)]
+    http_endpoint_enabled: bool,
+
+    /// TCP port to serve HTTP requests
+    #[arg(long, default_value_t = 8080)]
+    http_endpoint_port: u16,
+}
+
+#[derive(Default, Parser, Debug)]
+struct LurkProxyServerConfig {
+    /// Proxy server TCP port to listen on
+    #[arg(short = 'p', long, default_value_t = 1080)]
     proxy_port: u16,
 
-    /// Proxy IPv4 address to listen on
-    #[clap(short = 'i', long, default_value = "0.0.0.0")]
+    /// Proxy server IPv4 address to listen on
+    #[arg(short = 'i', long, default_value = "0.0.0.0")]
     proxy_ipv4: Option<Ipv4Addr>,
 }
 
 impl LurkConfig {
     pub fn server_tcp_bind_addr(&self) -> SocketAddr {
-        SocketAddr::new(
-            IpAddr::V4(self.proxy_ipv4.expect("IPv4 should have correct format")),
-            self.proxy_port,
-        )
+        let port = self.proxy_server_config.proxy_port;
+        let ipv4 = self.proxy_server_config.proxy_ipv4.expect("IPv4 should have correct format");
+
+        SocketAddr::new(IpAddr::V4(ipv4), port)
+    }
+
+    pub fn http_endpoint_bind_addr(&self) -> Option<SocketAddr> {
+        if !self.http_endpoint_config.http_endpoint_enabled {
+            return None;
+        }
+
+        let ipv4 = Ipv4Addr::new(0, 0, 0, 0);
+        let port = self.http_endpoint_config.http_endpoint_port;
+
+        Some(SocketAddr::new(IpAddr::V4(ipv4), port))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
+use log::error;
 use log4rs::config::Deserializers;
 use lurk::{
+    api::LurkHttpEndpoint,
     config::{self, LurkConfig},
     server::LurkServer,
 };
@@ -11,11 +13,21 @@ async fn main() -> Result<()> {
     // Initialize logging
     log4rs::init_file(config::LOG4RS_CONFIG_FILE_PATH, Deserializers::default()).unwrap();
 
-    // Parse server config
-    let config = LurkConfig::parse();
+    // Parse config
+    let lurk_config = LurkConfig::parse();
 
     // Create proxy server instance. It will handle incoming connection in async. fashion.
-    let server = LurkServer::new(config.server_tcp_bind_addr());
+    let server = LurkServer::new(lurk_config.server_tcp_bind_addr());
+
+    // Spin up HTTP endpoint if enabled
+    if let Some(http_endpoint_bind_addr) = lurk_config.http_endpoint_bind_addr() {
+        let http_endpoint = LurkHttpEndpoint::new(http_endpoint_bind_addr);
+        tokio::spawn(async move {
+            if let Err(err) = http_endpoint.run().await {
+                error!("Error occured while HTTP endpoint was running: {}", err);
+            }
+        });
+    }
 
     // Bind and serve clients "forever"
     server.run().await?;
