d71b6f7b2fdd39910cd5911cc8c4a12b9c631fa1
Boris S <boris.works@hotmail.com>
2024-08-13 11:54:06 +0300

    [Fix] Use relative URI path for outbound requests
    This is to prevent 414 HTTP errors. Also, error handling is adjusted.

--- src/server/handlers/http.rs
+++ src/server/handlers/http.rs
@@ -22,7 +22,7 @@ use tokio::net::TcpStream;
 pub struct LurkHttpHandler {}
 
 impl LurkHttpHandler {
-    async fn serve_request(request: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>> {
+    async fn serve_request(mut request: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>> {
         // Dump full request data if trace is enabled
         if log_enabled!(log::Level::Trace) {
             trace!("{:?}", request);
@@ -34,42 +34,38 @@ impl LurkHttpHandler {
             let addr_str = match request.uri().authority() {
                 Some(str) => str.to_string(),
                 None => {
-                    let err_msg = format!("CONNECT host is not socket addr: {:?}", request.uri());
-                    let mut response = Response::new(Self::full(err_msg));
-                    *response.status_mut() = StatusCode::BAD_REQUEST;
-                    return Ok(response);
+                    error!("CONNECT host is not socket addr: {:?}", request.uri());
+                    return Ok(Self::bad_request());
+                }
+            };
+
+            let mut outbound = match tcp::establish_tcp_connection(addr_str).await {
+                Ok(outbound) => outbound,
+                Err(err) => {
+                    error!("Failed to establish outbound TCP connection: {}", err);
+                    return Ok(Self::server_error());
                 }
             };
 
             tokio::spawn(async move {
                 // Upgrage HTTP connection.
-                let upgraded = match hyper::upgrade::on(request).await {
-                    Ok(upgraded) => upgraded,
+                let mut inbound = match hyper::upgrade::on(request).await {
+                    Ok(upgraded) => TokioIo::new(upgraded),
                     Err(err) => {
                         error!("HTTP upgrade error: {}", err);
                         return;
                     }
                 };
 
-                // On successful upgrade, establish remote TCP connection
-                // and start data relaying.
-                match tcp::establish_tcp_connection(addr_str).await {
-                    Ok(mut outbound) => {
-                        let mut inbdound = TokioIo::new(upgraded);
-                        let mut tunnel = LurkTunnel::new(&mut inbdound, &mut outbound);
+                let mut tunnel = LurkTunnel::new(&mut inbound, &mut outbound);
 
-                        // Start tunnel.
-                        if let Err(err) = tunnel.run().await {
-                            error!("Error occurred while tunnel was running: {}", err);
-                        }
-                    }
-                    Err(err) => {
-                        error!("Failed to establish outbound TCP connection: {}", err);
-                    }
+                // Start tunnel.
+                if let Err(err) = tunnel.run().await {
+                    error!("Error occurred while tunnel was running: {}", err);
                 }
             });
 
-            Ok(Response::new(Self::empty()))
+            Ok(Self::ok())
         } else {
             let host = request.uri().host().ok_or(anyhow!("HTTP request has no host"))?;
             let port = request.uri().port_u16().unwrap_or(80);
@@ -77,7 +73,11 @@ impl LurkHttpHandler {
             let stream = TcpStream::connect((host, port)).await?;
             let io = TokioIo::new(stream);
 
-            let (mut sender, conn) = client::conn::http1::Builder::new().handshake(io).await?;
+            let (mut sender, conn) = client::conn::http1::Builder::new()
+                .preserve_header_case(true)
+                .title_case_headers(true)
+                .handshake(io)
+                .await?;
 
             // Spawn a task to poll the connection and drive the HTTP state.
             tokio::spawn(async move {
@@ -86,8 +86,12 @@ impl LurkHttpHandler {
                 }
             });
 
+            // Use only path for outbound request to eliminate possible 414 "URI Too Long" http error.
+            *request.uri_mut() = request.uri().path().parse()?;
+
             // Send request on associated connection.
             let response = sender.send_request(request).await?;
+            trace!("{:?}", response);
 
             Ok(response.map(|r| r.boxed()))
         }
@@ -97,21 +101,42 @@ impl LurkHttpHandler {
     // Routines taken from example of proxy implementation based on hyper:
     // https://github.com/hyperium/hyper/blob/master/examples/http_proxy.rs
     //
-    fn empty() -> BoxBody<Bytes, hyper::Error> {
+    fn empty_body() -> BoxBody<Bytes, hyper::Error> {
         Empty::<Bytes>::new().map_err(|never| match never {}).boxed()
     }
 
-    fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    #[allow(dead_code)]
+    fn full_body<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
         Full::new(chunk.into()).map_err(|never| match never {}).boxed()
+    }
+
+    ///
+    /// HTTP responses.
+    ///
+    fn bad_request() -> Response<BoxBody<Bytes, hyper::Error>> {
+        Self::response(Self::empty_body(), StatusCode::BAD_REQUEST)
+    }
+
+    fn server_error() -> Response<BoxBody<Bytes, hyper::Error>> {
+        Self::response(Self::empty_body(), StatusCode::INTERNAL_SERVER_ERROR)
+    }
+
+    fn ok() -> Response<BoxBody<Bytes, hyper::Error>> {
+        Self::response(Self::empty_body(), StatusCode::OK)
+    }
+
+    fn response<T>(body: T, status: StatusCode) -> Response<T> {
+        Response::builder().status(status).body(body).expect("HTTP response was not built")
     }
 }
 
 #[async_trait]
 impl LurkTcpConnectionHandler for LurkHttpHandler {
     async fn handle(&mut self, conn: LurkTcpConnection) -> Result<()> {
-        let io = TokioIo::from(conn);
         server::conn::http1::Builder::new()
-            .serve_connection(io, service_fn(LurkHttpHandler::serve_request))
+            .preserve_header_case(true)
+            .title_case_headers(true)
+            .serve_connection(TokioIo::from(conn), service_fn(LurkHttpHandler::serve_request))
             .with_upgrades()
             .await
             .map_err(anyhow::Error::from)
